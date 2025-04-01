efec810ffc4be330ba1d88f412a9b1138b525336
Chea Ilong <155554832+Chea-Ilong@users.noreply.github.com>
2025-04-02 01:15:01 +0700

    show me

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    output: 'export',
+    distDir: 'out', // This makes Next.js output to an 'out' directory
+    images: {
+      unoptimized: true,
+    },
+    trailingSlash: true,
+  }
